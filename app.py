2701037e96702beb448ac27b885b1cdbdf3eaa2e
AP <abhishekpradhan82@gmail.com>
2019-04-13 15:27:23 +0530

    intial commit

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
+from flask import Flask,render_template,request
 
-print('Hello World')
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+
+if __name__ == "__main__" :
+    app.run()
